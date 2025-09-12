31165d59270b1195e1f423779e54c173b7859f35
Jameson210802 <jamie.anderson210802@gmail.com>
2025-09-11 20:32:49 -0500

    exported some pictures

--- HW_1.R
+++ HW_1.R
@@ -1,30 +1,63 @@
 # HW 1
 # Jamie Anderson netid: jla656
 
-
+library(ggplot2)
+options(scipen = 999)
 ## part a ##
 
 ## summary stats of price ##
 
 summary(kingCountyHouses$price)
 
-options(scipen = 999) # disable scientific notation 
+ # disable scientific notation 
 
 ## graphs of price ##
 
+
+price_box_stats <- boxplot.stats(kingCountyHouses$price)
+price_out <- length(price_box_stats$out)
+
+price_out_label <- paste("Number of Outliers:", price_out)
+
 boxplot(kingCountyHouses$price, main="Price of houses",ylab="Price")
+
+mtext(price_out_label,side= 1, line = 0.5)
+
+
+
+
+
+
 hist(kingCountyHouses$price,main="Histogram of Price of Houses",xlab="Price")
 
-
-
+density_price <- density(kingCountyHouses$price)
+plot(density_price, main="Density plot: Price of homes")
+polygon(density_price,col="skyblue",border="black")
 ## Summary stats of Sqft ##
 summary(kingCountyHouses$sqft)
 
-## graphs of Price ##
+
+density_sqft <- density(kingCountyHouses$sqft)
+plot(density_sqft, main="Density plot: Square feet of homes")
+polygon(density_sqft,col="skyblue",border="black")
+
+## graphs of sqft ##
 boxplot(kingCountyHouses$sqft,main="Square feet of houses",ylab="Square feet")
 hist(kingCountyHouses$sqft,main="Histogram of square feet of houses",ylab="Square feet")
 
 plot(kingCountyHouses$sqft,kingCountyHouses$price,ylab="Price",xlab="Square feet")
+
+
+sqft_box_stats <- boxplot.stats(kingCountyHouses$sqft)
+sqft_out <- length(sqft_box_stats$out)
+
+sqft_out_label <- paste("Number of Outliers:", price_out)
+
+boxplot(kingCountyHouses$sqft, main="Square feet of houses",ylab="Square feet")
+
+mtext(sqft_out_label,side= 1, line = 0.5)
+
+
 
 
 
@@ -33,21 +66,16 @@ plot(kingCountyHouses$sqft,kingCountyHouses$price,ylab="Price",xlab="Square feet
 
 
 
-scatter.smooth(x=kingCountyHouses$sqft,y=kingCountyHouses$price,main="Price ~ Square feet")
+scatter.smooth(x=kingCountyHouses$sqft,y=kingCountyHouses$price,main="Price ~ Square feet",lpars = list(col = "red"))
 boxplot(kingCountyHouses$price,kingCountyHouses$sqft)
 
-##summary(lm(kingCountyHouses$price~kingCountyHouses$sqft))
+summary(lm(kingCountyHouses$price~kingCountyHouses$sqft))
 
 lm.model1 <- lm(formula = kingCountyHouses$price~kingCountyHouses$sqft)
 
 par(mfrow=c(2,2))
 gvlma::gvlma(lm.model1)
 plot(lm.model1)
-
-##plot(linearpricesqft.mod)
-
-
-
 
 
 
@@ -63,18 +91,51 @@ lm.model2
 # storing coefficent and intercept from lm.model2
 
 model2_coefficients <- coef(lm.model2)
+coef(lm.model2)
 
 intercept_model2 <- model2_coefficients["(Intercept)"]
 model2_sqft_coefficent <- model2_coefficients["kingCountyHouses$sqft"]
 
-y <- intercept_model2 + (model2_sqft_coefficent *100001)
+model2_price_1 <- intercept_model2 + (model2_sqft_coefficent *10000)
 
-y
-
-
+model2_price_2 <- intercept_model2 + (model2_sqft_coefficent * 10001)
 
 
+# part e. check back later. 
+diff_price <- log(model2_price_1) - log(model2_price_2)
 
+diff_price
+
+
+
+
+## part f
+
+
+
+## part e
+
+logsqft <- log(kingCountyHouses$sqft)
+
+lm.model3 <- lm(formula = kingCountyHouses$price ~ logsqft)
+
+model3_coefficients<- coef(lm.model3)
+
+
+intercept_model3 <- model3_coefficients["(Intercept)"]
+model3_logsqft_coefficent <- model3_coefficients["logsqft"]
+
+
+
+model3_regression_equation_1 <-intercept_model3 + (model3_logsqft_coefficent * 10000)
+model3_regression_equation_2 <-intercept_model3 + (model3_logsqft_coefficent * 10001)
+lm.model3
+
+model3_regression_equation_1
+model3_regression_equation_2
+
+
+summary(lm.model3)
 
 
 
